f7a690f840d2d0af4acee8d45dd2f3940db29fa6
davidsugianto <davidmufc98@gmail.com>
2019-02-13 14:26:26 +0700

    dev test T01

---
+++ T01.py
@@ -0,0 +1,10 @@
+a = raw_input("masukkan No handphone <spasi> nama lengkap <spasi> usia <spasi> email : ")
+data = a.split(" ")
+
+print "Nomor Handphone : "+ data[0]
+print "Nama : "+ data[1] + " " + data[2]
+print "Usia : "+ data[3] +" Tahun"
+print "E-mail : "+ data[4]
+
+
+
